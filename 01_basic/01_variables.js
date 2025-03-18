8d2e53cb0e3fce92463e4242e301e3d05d945658
aarispathan <140056433+aarispathan@users.noreply.github.com>
2025-03-18 08:29:08 +0000

    declared variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accontId = 23557
+let accountEmail = "aaris@1234gmail.com"
+var accountPassword = "fbdg214"
+AccountCity = "Ahmedabad"
+let accountState;
+
+// accontId = 123 this is not allowed because we already declared this value in const
+accountEmail = "ak@gmail.com"
+accountPassword = "09878"
+AccountCity = "gandhinagar"
+
+console.log(accontId);
+
+console.table([accontId, accountEmail, accountPassword, AccountCity, accountState]);
+
