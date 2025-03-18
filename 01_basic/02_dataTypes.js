fa3c69fac0adba473cf623cc38abc06ff68a9c0b
aarispathan <140056433+aarispathan@users.noreply.github.com>
2025-03-18 09:06:39 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"
+
+let name = "Aaris" /* string can be used in '' or "" */
+
+let age = 21 /* number => 2 to power 53 */
+
+let isLoggedIn = false /* boolean is true or flase   */
+
+let state = null
+
+/* this is primitive datatypes*/
+
+// string => '' or ""
+// symbol => unique
+// number => 2 to power 53
+// null => standalone value and null is object
+// bigInt
+// boolean => true or flase
+// undefined => 
+
+
+console.log(typeof age);
+console.log(typeof undefined); //undefind
+console.log(typeof null);  //object
+
+
+
