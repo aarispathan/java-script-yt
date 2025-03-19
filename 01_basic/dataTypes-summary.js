004aafcaa0f5d54bba69505ef3d45abade4689fd
aarispathan <140056433+aarispathan@users.noreply.github.com>
2025-03-19 17:08:49 +0000

    datatype summary

---
+++ 01_basic/dataTypes-summary.js
@@ -0,0 +1,46 @@
+//JavaScript is a dynamic language.
+// typeof can find data type
+// Primitive Data Types
+
+//  there 7 types of primitive data  types
+
+// 1 string => "" , ''
+// let name = "aaris";
+// let email = 'ak';
+
+
+// 2 symbol => using for value unique
+// const id = Symbol('999')
+
+// 3 boolean => true or false
+// 4 bigInt => use for long number value
+const bigNumber = 3467860939409560943409346879567n
+console.log(typeof bigNumber);
+
+
+// 5 null => empty
+// 6 number
+// 7 undefined
+
+
+// Nonprimitibe Data Types (Reference)
+
+// Array  example
+// array always in square brakets
+// const myFrdName = ["Aaris", "Aaris", "Aaris"]
+
+
+
+// Object  example
+// object always in curly brakets
+// const mySelf = {
+//     name: "Aaris",
+//     age: 20,
+// }
+
+
+// fonctions example
+const myFunction = function (){
+    console.log("Aaris");
+}
+console.log(typeof (myFunction));
