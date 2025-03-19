74b41a0e7ff7fc85c0975e06045f01f6b9402a72
aarispathan <140056433+aarispathan@users.noreply.github.com>
2025-03-19 16:37:42 +0000

    comparision data types

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,18 @@
+// their value in boolean true or false
+
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+console.log(2 > "1");
+
+// == is check only value
+console.log("2" == 2);
+
+// === strict check is check value and data types
+console.log("2" === 2);
+
