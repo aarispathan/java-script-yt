dd681e65b2f4133aafdd937a46c7fb28756cd2aa
aarispathan <140056433+aarispathan@users.noreply.github.com>
2025-04-22 08:50:39 +0000

    learn about if else if

---
+++ 04_control_flow/one.js
@@ -0,0 +1,52 @@
+// if 
+const isUserLoggedIn = true
+
+// const temperature = 42
+
+// if ( temperature === 42){
+//     console.log("less then 50");
+// } else {
+//     console.log("temprature is greater than 50");
+    
+// }
+
+// console.log("execute");
+
+
+// const score = 200
+
+// if (score > 100){
+//     const power = "fly"
+//     console.log(`user power ${power}`); //this will run 
+// }
+// console.log(`user power ${power}`); this will throw an error because this is out of scope
+
+// const balance = 500
+
+// if (balance > 500) console.log("test"); , console.log("test1"); your code is run but this is wrong
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// }else if (balance < 750){
+//     console.log("less than 750");
+// }else if (balance < 900 ){
+//     console.log("less than 900");
+// } else{
+//     console.log("less than 1150");
+// }
+
+const userLoggedIn = true
+const debbitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debbitCard && 2==2 ) {
+        console.log("allow to buy items");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("logged in");
+    
+}
+
+// <, >, <=, >=, !=, ==, ===
